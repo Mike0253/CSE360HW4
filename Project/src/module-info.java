51228513a5094bd2110205e1cc3b1fed2f02c240
Mike <Mikeschiffman1@gmail.com>
2025-04-10 02:04:58 -0400

    Added code

---
+++ Project/src/module-info.java
@@ -0,0 +1,9 @@
+module FoundationCode {
+	requires javafx.controls;
+	requires java.sql;
+	requires javafx.graphics;
+	
+	requires org.junit.jupiter.api;
+
+	opens application to javafx.graphics, javafx.fxml;
+}
